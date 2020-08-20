732a75ce79bdaba69626db14620ec56379454967
Sam Seedy <sam.seed@softwire.com>
2020-08-20 18:43:38 +0100

    refactor: add methods to ViewModel class

--- viewModel.py
+++ viewModel.py
@@ -2,3 +2,12 @@ class ViewModel:
 
     def __init__(self, items):
         self.items = items
+
+    def get_to_do_items(self):
+        to_do_items = []
+        for item in self.items:
+                if self.items.status == "To Do":
+                    to_do_items.append(item)
+                else:
+                    continue
+            
