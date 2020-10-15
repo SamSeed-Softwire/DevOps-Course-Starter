e02370a620a0b4a8f766173e0bd6eb409dd053c4
Sam Seed <sam.seed@softwire.com>
2020-10-15 13:46:16 +0100

    feat(Docker): update shell script to include bind mount for Flask

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -7,32 +7,29 @@ docker stop $(docker container ls -aq)
 docker rm $(docker container ls -aq)
 
 
-### Build only
+### Build
 
 # Build dev image
-#docker build --target dev --tag=todo-app:dev .
+docker build --target dev --tag=todo-app:dev .
 
 # Build prod image
-#docker build --target prod --tag=todo-app:prod .
+docker build --target prod --tag=todo-app:prod .
 
 
-### Build and run
+### Run
 
 ## Non-interactive
 
 # Run non-interactive (dev)
-#docker build --target dev --tag=todo-app:dev .; docker run -d -p 5000:5000 --env-file .env todo-app:dev
+#docker run -d -p 5000:5000 --mount type=bind,source="$(pwd)"/app,target=/app/app/ --env-file .env todo-app:dev
 
 # Run non-interactive (prod)
-docker build --target prod --tag=todo-app:prod .; docker run -d -p 5000:5000 --env-file .env todo-app:prod
+#docker run -d -p 5000:5000 --env-file .env todo-app:prod
 
-
-### Run only
-
-## Interactive
-
-# Run interactive (dev) with mount
-#docker run -it -p 5000:5000 --env-file .env --mount type=bind,src="$pwd",dst=/app2 todo-app:dev /bin/bash
+## Interactive (currently haven't worked out how to enter interactive mode when running this shell script on Windows)
 
 # Run interactive (dev)
-#docker run -it -p 5000:5000 --env-file .env todo-app:dev /bin/bash
+#docker run -it -p 5000:5000 --mount type=bind,source="$(pwd)"/app,target=/app/app/ --env-file .env todo-app:dev /bin/bash
+
+# Run interactive (prod)
+#docker run -it -p 5000:5000 --env-file .env todo-app:prod /bin/bash
