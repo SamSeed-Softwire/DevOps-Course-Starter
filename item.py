789766ab3e37f2592c750a9bffb67ab19ec58579
Sam Seed <sam.seed@softwire.com>
2020-11-05 18:15:30 +0000

    refactor(class): update equality __eq__ operators for Item and ViewModel

--- item.py
+++ item.py
@@ -4,3 +4,9 @@ class Item:
         self.id = id
         self.title = title
         self.status = status
+    
+    def __eq__(self, other):
+        """Overrides the default implementation."""
+        if type(self) == type(other):
+            return self.__dict__ == other.__dict__
+        return False
