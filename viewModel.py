a006a8857d506cabf4f5308e2e234b708b1f6044
Sam Seed <sam.seed@softwire.com>
2020-11-05 11:51:41 +0000

    refactor(view model): add getter and setter to ViewModel class

--- viewModel.py
+++ viewModel.py
@@ -3,6 +3,14 @@ class ViewModel:
     def __init__(self, items):
         self.items = items
 
+    @property
+    def items(self):
+        return self._items
+    
+    @items.setter
+    def items(self, items):
+        self._items = items
+    
     def get_to_do_items(self):
         to_do_items = []
         for item in self.items:
