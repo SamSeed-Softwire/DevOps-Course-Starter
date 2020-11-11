f601ebeae58b5059b3d854c32e5cd47a037e9049
Sam Seed <sam.seed@softwire.com>
2020-11-11 17:31:30 +0000

    feat(Item): add last_modified variable; update Trello functions

--- item.py
+++ item.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 class Item:
 
-    def __init__(self, id, title, status):
+    def __init__(self, id, title, status, last_modified: datetime):
         self.id = id
         self.title = title
         self.status = status
+        self.last_modified = last_modified
     
     def __eq__(self, other):
         """Overrides the default implementation."""
