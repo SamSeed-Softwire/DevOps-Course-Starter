a6fbb1843c9573f7c78debccfde30236b25196c5
Sam Seed <sam.seed@softwire.com>
2020-11-11 16:11:13 +0000

    feat(Flask): add 'start item' (i.e. 'move to doing') route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import session_items as session
 from flask import Flask, render_template, request, redirect, url_for
-from trello_functions import get_items, add_item_to_list, move_to_done
+from trello_functions import get_items, add_item_to_list, move_to_doing, move_to_done
 from viewModel import ViewModel
 
 app = Flask(__name__)
@@ -17,6 +17,11 @@ def add_item():
     item = request.form.get('item_name')
     add_item_to_list(item, '5efc77d5ce13a25c8b6bf9c1')
     return redirect('/') 
+
+@app.route('/start-item/<item_id>', methods = ['POST'])
+def start_item(item_id):
+    move_to_doing(item_id)
+    return redirect('/')
 
 @app.route('/complete-item/<item_id>', methods = ['POST'])
 def complete_item(item_id):
