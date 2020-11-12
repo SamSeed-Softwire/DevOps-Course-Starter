6feb22c98dd7b4e7b0aaca5694021ba31858edf6
Sam Seed <sam.seed@softwire.com>
2020-11-12 18:04:06 +0000

    refactor(class): create TrelloData class

---
+++ trelloData.py
@@ -0,0 +1,32 @@
+import requests
+import os
+
+AUTH_PARAMS_KEY = os.environ.get('AUTH_PARAMS_KEY')
+AUTH_PARAMS_TOKEN = os.environ.get('AUTH_PARAMS_TOKEN')
+auth_params = {'key' : AUTH_PARAMS_KEY, 'token' : AUTH_PARAMS_TOKEN}
+
+class TrelloData:
+
+    def __init__(self, board_id: str):
+    
+        self.idList_todo = get_idList(board_id, "To Do")
+        self.idList_doing = get_idList(board_id, "Doing")
+        self.idList_done = get_idList(board_id, "Done")
+        self.lists = get_lists(board_id)
+        self.cards = get_cards(board_id)
+        self.board_id = board_id
+
+
+def get_lists(board_id: str):
+    return requests.get(f'https://api.trello.com/1/boards/{board_id}/lists', params = auth_params).json()
+
+def get_cards(board_id: str):
+    return requests.get(f'https://api.trello.com/1/boards/{board_id}/cards', params = auth_params).json()
+
+def get_idList(board_id: str, name: str):
+    lists = get_lists(board_id)
+    # QQ: Need test for when multiple lists have the same name.
+    for list in lists:
+        if list["name"] == name:
+            return list["id"]
+    raise ValueError(f'No list exists with the name "{name}".')
