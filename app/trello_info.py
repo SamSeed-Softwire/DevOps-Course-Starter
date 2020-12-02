5cc5f7db2b3658a71bc6931eebfa52461dd29522
Sam Seed <sam.seed@softwire.com>
2020-12-02 17:39:37 +0000

    refactor(class): add Trello auth params to Trello class constructors

--- app/trello_info.py
+++ app/trello_info.py
@@ -1,37 +1,31 @@
 import requests
-import os
-
-auth_params_key = os.environ.get('AUTH_PARAMS_KEY')
-auth_params_token = os.environ.get('AUTH_PARAMS_TOKEN')
-auth_params = {'key' : auth_params_key, 'token' : auth_params_token}
-
 
 class TrelloData:
 
-    def __init__(self, board_id: str):
+    def __init__(self, board_id: str, auth_params):
 
-        self.lists = get_lists(board_id)
-        self.cards = get_cards(board_id)
+        self.lists = get_lists(board_id, auth_params)
+        self.cards = get_cards(board_id, auth_params)
 
 
 class TrelloIDs:
 
-    def __init__(self, board_id: str):
+    def __init__(self, board_id: str, auth_params):
 
         self.board_id = board_id
-        self.idList_todo = get_idList(board_id, "To Do")
-        self.idList_doing = get_idList(board_id, "Doing")
-        self.idList_done = get_idList(board_id, "Done")
+        self.idList_todo = get_idList(board_id, "To Do", auth_params)
+        self.idList_doing = get_idList(board_id, "Doing", auth_params)
+        self.idList_done = get_idList(board_id, "Done", auth_params)
 
 
-def get_lists(board_id: str):
+def get_lists(board_id: str, auth_params):
     return requests.get(f'https://api.trello.com/1/boards/{board_id}/lists', params = auth_params).json()
 
-def get_cards(board_id: str):
+def get_cards(board_id: str, auth_params):
     return requests.get(f'https://api.trello.com/1/boards/{board_id}/cards', params = auth_params).json()
 
-def get_idList(board_id: str, name: str):
-    lists = get_lists(board_id)
+def get_idList(board_id: str, name: str, auth_params):
+    lists = get_lists(board_id, auth_params)
     # QQ: Need test for when multiple lists have the same name.
     for list in lists:
         if list["name"] == name:
