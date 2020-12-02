bccb6be8618c896e7a14e02c86e88db115699d94
Sam Seed <sam.seed@softwire.com>
2020-12-02 17:43:33 +0000

    refactor(method): create Flask app using new method: create_app

--- app/app.py
+++ app/app.py
@@ -2,36 +2,39 @@ from flask import Flask, render_template, request, redirect
 from trello_functions import get_items, add_item_to_todo, move_to_doing, move_to_done, delete_all_items
 from view_model import ViewModel
 
+def create_app():
 
-app = Flask(__name__)
-app.config.from_object('flask_config.Config')
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    items = get_items()
-    view_model = ViewModel(items)
-    return render_template('index.html', view_model = view_model)
+    @app.route('/')
+    def index():
+        items = get_items()
+        view_model = ViewModel(items)
+        return render_template('index.html', view_model = view_model)
 
-@app.route('/add-item', methods = ['POST'])
-def add_item():
-    item = request.form.get('item_name')
-    add_item_to_todo(item)
-    return redirect('/') 
+    @app.route('/add-item', methods = ['POST'])
+    def add_item():
+        item = request.form.get('item_name')
+        add_item_to_todo(item)
+        return redirect('/')
 
-@app.route('/start-item/<item_id>', methods = ['POST'])
-def start_item(item_id):
-    move_to_doing(item_id)
-    return redirect('/')
+    @app.route('/start-item/<item_id>', methods = ['POST'])
+    def start_item(item_id):
+        move_to_doing(item_id)
+        return redirect('/')
 
-@app.route('/complete-item/<item_id>', methods = ['POST'])
-def complete_item(item_id):
-    move_to_done(item_id)
-    return redirect('/')
+    @app.route('/complete-item/<item_id>', methods = ['POST'])
+    def complete_item(item_id):
+        move_to_done(item_id)
+        return redirect('/')
 
-@app.route('/delete-all-items', methods = ['POST'])
-def delete_items():
-    delete_all_items()
-    return redirect('/')
+    @app.route('/delete-all-items', methods = ['POST'])
+    def delete_items():
+        delete_all_items()
+        return redirect('/')
 
+    return app
+
+app = create_app()
 if __name__ == '__main__':
     app.run()
