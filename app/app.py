29e237ace91ebe0f5424077a62b702300d81c201
Sam Seed <sam.seed@softwire.com>
2021-01-08 19:19:50 +0000

    refactor: convert trello_functions to TrelloClient class

--- app/app.py
+++ app/app.py
@@ -1,36 +1,39 @@
 from flask import Flask, render_template, request, redirect
-from app.trello_functions import get_items, add_item_to_todo, move_to_doing, move_to_done, delete_all_items
+
+from app.trello_client import TrelloClient
 from app.view_model import ViewModel
 
 def create_app():
 
     app = Flask(__name__)
+    trello_client = TrelloClient()
 
     @app.route('/')
     def index():
-        items = get_items()
+        trello_client.refresh_items()
+        items = trello_client.items
         view_model = ViewModel(items)
         return render_template('index.html', view_model = view_model)
 
     @app.route('/add-item', methods = ['POST'])
     def add_item():
         item = request.form.get('item_name')
-        add_item_to_todo(item)
+        trello_client.add_item_to_todo(item)
         return redirect('/')
 
     @app.route('/start-item/<item_id>', methods = ['POST'])
     def start_item(item_id):
-        move_to_doing(item_id)
+        trello_client.move_to_doing(item_id)
         return redirect('/')
 
     @app.route('/complete-item/<item_id>', methods = ['POST'])
     def complete_item(item_id):
-        move_to_done(item_id)
+        trello_client.move_to_done(item_id)
         return redirect('/')
 
     @app.route('/delete-all-items', methods = ['POST'])
     def delete_items():
-        delete_all_items()
+        trello_client.delete_all_items()
         return redirect('/')
 
     return app
--- app/trello_ids.py
+++ app/trello_ids.py
@@ -1,13 +1,5 @@
 import requests
 
-class TrelloData:
-
-    def __init__(self, board_id: str, auth_params):
-
-        self.lists = get_lists(board_id, auth_params)
-        self.cards = get_cards(board_id, auth_params)
-
-
 class TrelloIDs:
 
     def __init__(self, board_id: str, auth_params):
@@ -20,9 +12,6 @@ class TrelloIDs:
 
 def get_lists(board_id: str, auth_params):
     return requests.get(f'https://api.trello.com/1/boards/{board_id}/lists', params = auth_params).json()
-
-def get_cards(board_id: str, auth_params):
-    return requests.get(f'https://api.trello.com/1/boards/{board_id}/cards', params = auth_params).json()
 
 def get_idList(board_id: str, name: str, auth_params):
     lists = get_lists(board_id, auth_params)
