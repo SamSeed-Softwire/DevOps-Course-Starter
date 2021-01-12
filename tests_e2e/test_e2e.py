3c29a17afd5931f5decdd777618855837e4e1280
Sam Seed <sam.seed@softwire.com>
2021-01-12 18:53:42 +0000

    test(e2e): assert page title is correct

--- tests_e2e/test_e2e.py
+++ tests_e2e/test_e2e.py
@@ -53,3 +53,8 @@ def delete_trello_board(id):
 def driver():
     with webdriver.Firefox() as driver:
         yield driver
+
+def test_task_journey(driver, test_app):
+    driver.get('http://localhost:5000/')
+
+    assert driver.title == 'To-Do App'
