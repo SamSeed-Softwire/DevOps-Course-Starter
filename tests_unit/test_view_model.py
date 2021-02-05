7a6033c452ab83feee898081370f5dc7f943622a
Sam Seed <sam.seed@softwire.com>
2021-02-05 10:13:37 +0000

    test(unit): update statuses to match new db list names

--- tests_unit/test_view_model.py
+++ tests_unit/test_view_model.py
@@ -2,23 +2,25 @@ from datetime import datetime, timedelta
 from application.view_model import ViewModel
 from application.item import Item
 
+
 dummy_id = 'id'
 dummy_title = 'title'
 dummy_status = 'status'
 dummy_last_modified = datetime(1, 1, 1)
 
+
 def test_todo_items():
 
     items = [
-         Item(dummy_id, dummy_title, 'To Do', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Status is not "To Do"', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'To Do', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'todo-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'Status is not "todo-items"', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'todo-items', dummy_last_modified)
     ]
     item_view_model = ViewModel(items)
     todo_items = item_view_model.todo_items
     assert todo_items == [
-         Item(dummy_id, dummy_title, 'To Do', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'To Do', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'todo-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'todo-items', dummy_last_modified)
     ]
 
     empty_items = []
@@ -26,18 +28,19 @@ def test_todo_items():
     empty_todo_items = empty_item_view_model.todo_items
     assert empty_todo_items == []
 
+
 def test_doing_items():
 
     items = [
-         Item(dummy_id, dummy_title, 'Doing', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Status is not "Doing"', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Doing', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'doing-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'Status is not "doing-items"', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'doing-items', dummy_last_modified)
     ]
     item_view_model = ViewModel(items)
     doing_items = item_view_model.doing_items
     assert doing_items == [
-         Item(dummy_id, dummy_title, 'Doing', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Doing', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'doing-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'doing-items', dummy_last_modified)
     ]
 
     empty_items = []
@@ -49,15 +52,15 @@ def test_doing_items():
 def test_done_items():
 
     items = [
-         Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Status is not "Done"', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'Status is not "done-items"', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
     ]
     item_view_model = ViewModel(items)
     done_items = item_view_model.done_items
     assert done_items == [
-         Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
     ]
 
     empty_items = []
@@ -69,47 +72,49 @@ def test_done_items():
 def test_show_all_done_items():
 
     items_x4 = [
-         Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)        
+         Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
     ]
     item_view_model_x4 = ViewModel(items_x4)
     assert item_view_model_x4.show_all_done_items == True
 
     items_x5 = [
-         Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)        
-        ,Item(dummy_id, dummy_title, 'Done', dummy_last_modified)
+         Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
+        ,Item(dummy_id, dummy_title, 'done-items', dummy_last_modified)
     ]
     item_view_model_x5 = ViewModel(items_x5)
     assert item_view_model_x5.show_all_done_items == False
 
+
 def test_recent_done_items():
     today = datetime.today()
     items = [
-         Item(dummy_id, dummy_title, 'Done', today + timedelta(days = -1))
-        ,Item(dummy_id, dummy_title, 'Done', today)
-        ,Item(dummy_id, dummy_title, 'Status is not "Done"', today)
-        ,Item(dummy_id, dummy_title, 'Done', today + timedelta(days = +1))
+         Item(dummy_id, dummy_title, 'done-items', today + timedelta(days = -1))
+        ,Item(dummy_id, dummy_title, 'done-items', today)
+        ,Item(dummy_id, dummy_title, 'Status is not "done-items"', today)
+        ,Item(dummy_id, dummy_title, 'done-items', today + timedelta(days = +1))
     ]
     item_view_model = ViewModel(items)
     recent_done_items = item_view_model.recent_done_items
     assert recent_done_items == [
-         Item(dummy_id, dummy_title, 'Done', today)
+         Item(dummy_id, dummy_title, 'done-items', today)
     ]
+
 
 def test_older_done_items():
     today = datetime.today()
     items = [
-         Item(dummy_id, dummy_title, 'Done', today + timedelta(days = -1))
-        ,Item(dummy_id, dummy_title, 'Status is not "Done"', today + timedelta(days = -1))
-        ,Item(dummy_id, dummy_title, 'Done', today)
+         Item(dummy_id, dummy_title, 'done-items', today + timedelta(days = -1))
+        ,Item(dummy_id, dummy_title, 'Status is not "done-items"', today + timedelta(days = -1))
+        ,Item(dummy_id, dummy_title, 'done-items', today)
     ]
     item_view_model = ViewModel(items)
     older_done_items = item_view_model.older_done_items
     assert older_done_items == [
-         Item(dummy_id, dummy_title, 'Done', today + timedelta(days = -1))
+         Item(dummy_id, dummy_title, 'done-items', today + timedelta(days = -1))
     ]
