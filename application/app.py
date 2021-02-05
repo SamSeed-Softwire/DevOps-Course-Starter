7644ae8b347e78a6a47d8e6703e9bb6591fe736d
Sam Seed <sam.seed@softwire.com>
2021-02-05 12:37:33 +0000

    refactor(class): rename TrelloClient to MongoClient

--- application/app.py
+++ application/app.py
@@ -1,45 +1,45 @@
 from flask import Flask, render_template, request, redirect
 
-from application.trello_client import TrelloClient
+from application.mongo_client import MongoClient
 from application.view_model import ViewModel
 
 
 def create_app():
 
     app = Flask(__name__)
-    trello_client = TrelloClient()
+    mongo_client = MongoClient()
 
     @app.route('/')
     def index():
-        trello_client.refresh_items()
-        items = trello_client.items
+        mongo_client.refresh_items()
+        items = mongo_client.items
         view_model = ViewModel(items)
         return render_template('index.html', view_model = view_model)
 
     @app.route('/add-item', methods = ['POST'])
     def add_item():
         item_name = request.form.get('item_name')
-        trello_client.add_item(item_name, 'todo-items')
+        mongo_client.add_item(item_name, 'todo-items')
         return redirect('/')
 
     @app.route('/start-item/<item_id>', methods = ['POST'])
     def start_item(item_id):
-        trello_client.move_item(item_id, 'todo-items', 'doing-items')
+        mongo_client.move_item(item_id, 'todo-items', 'doing-items')
         return redirect('/')
 
     @app.route('/complete-item/<item_id>', methods = ['POST'])
     def complete_item(item_id):
-        trello_client.move_item(item_id, 'doing-items', 'done-items')
+        mongo_client.move_item(item_id, 'doing-items', 'done-items')
         return redirect('/')
 
     @app.route('/uncomplete-item/<item_id>', methods = ['POST'])
     def uncomplete_item(item_id):
-        trello_client.move_item(item_id, 'done-items', 'doing-items')
+        mongo_client.move_item(item_id, 'done-items', 'doing-items')
         return redirect('/')
 
     @app.route('/delete-all-items', methods = ['POST'])
     def delete_items():
-        trello_client.delete_all_items()
+        mongo_client.delete_all_items()
         return redirect('/')
 
     return app
