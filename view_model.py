8b7717d06c27b4505cf2de3269bbba6725b749c1
Sam Seed <sam.seed@softwire.com>
2021-02-05 16:18:50 +0000

    refactor(filenames): rename viewModel to view_model

---
+++ view_model.py
@@ -0,0 +1,39 @@
+from datetime import date, timedelta
+from app.item import Item
+class ViewModel:
+
+    def __init__(self, items):
+        self._items = items
+
+    # Properties
+
+    @property
+    def todo_items(self):
+        return [item for item in self._items if item.status == 'To Do']
+
+    @property
+    def doing_items(self):
+        return [item for item in self._items if item.status == 'Doing']
+
+    @property
+    def done_items(self):
+        return [item for item in self._items if item.status == 'Done']
+
+    @property
+    def show_all_done_items(self):
+        return len(self.done_items) <= 4
+
+    @property
+    def recent_done_items(self):
+        return [item for item in self._items if item.status == 'Done' if item.last_modified.date() == date.today()]
+
+    @property
+    def older_done_items(self):
+        return [item for item in self._items if item.status == 'Done' if item.last_modified.date() <= date.today() + timedelta(days = -1)]
+
+    # Overriding the default class equivalence implementation
+    def __eq__(self, other):
+        """Overrides the default implementation."""
+        if type(self) == type(other):
+            return self.items == other.items
+        return False
