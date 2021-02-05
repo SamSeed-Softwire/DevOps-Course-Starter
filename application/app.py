7dcd786e7bf954c82f0182d107ad226e715e8a91
Sam Seed <sam.seed@softwire.com>
2021-02-05 10:07:32 +0000

    feat(Flask): update Flask routes to match new MongoDB methods

--- application/app.py
+++ application/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect
 from application.trello_client import TrelloClient
 from application.view_model import ViewModel
 
+
 def create_app():
 
     app = Flask(__name__)
@@ -18,22 +19,22 @@ def create_app():
     @app.route('/add-item', methods = ['POST'])
     def add_item():
         item = request.form.get('item_name')
-        trello_client.add_item_to_todo(item)
+        trello_client.add_item(item, 'todo-items')
         return redirect('/')
 
     @app.route('/start-item/<item_id>', methods = ['POST'])
     def start_item(item_id):
-        trello_client.move_to_doing(item_id)
+        trello_client.move_item(item_id, 'todo-items', 'doing-items')
         return redirect('/')
 
     @app.route('/complete-item/<item_id>', methods = ['POST'])
     def complete_item(item_id):
-        trello_client.move_to_done(item_id)
+        trello_client.move_item(item_id, 'doing-items', 'done-items')
         return redirect('/')
 
     @app.route('/uncomplete-item/<item_id>', methods = ['POST'])
     def uncomplete_item(item_id):
-        trello_client.move_to_doing(item_id)
+        trello_client.move_item(item_id, 'done-items', 'doing-items')
         return redirect('/')
 
     @app.route('/delete-all-items', methods = ['POST'])
