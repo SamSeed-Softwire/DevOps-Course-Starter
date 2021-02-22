c8165e15fd1bb932c1e93071d326f10e8e915633
Sam Seed <sam.seed@softwire.com>
2021-02-22 12:21:56 +0000

    refactor(e2e): don't use usecwd option in find_dotenv

--- tests/tests_e2e/test_e2e.py
+++ tests/tests_e2e/test_e2e.py
@@ -15,7 +15,7 @@ import application.app as app
 def test_app():
 
     # Load the environment variables.
-    file_path = find_dotenv('.env', usecwd=True)
+    file_path = find_dotenv('.env')
     load_dotenv(file_path, override=True)
 
     temp_db = "temp_db"
