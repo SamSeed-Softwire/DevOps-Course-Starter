2bfc6219bd7384d50a1654e165244bfce048e98f
Sam Seed <sam.seed@softwire.com>
2021-04-01 16:23:57 +0100

    feat: add user management section in mongo_client

--- application/mongo_client.py
+++ application/mongo_client.py
@@ -4,6 +4,7 @@ import os
 import pymongo
 
 from application.item import Item
+from application.user import User
 
 class MongoClient:
 
@@ -15,6 +16,60 @@ class MongoClient:
         self.MONGO_TODO_APP_DATABASE = os.environ.get('MONGO_TODO_APP_DATABASE')
         self.client = pymongo.MongoClient(f"mongodb+srv://{self.MONGO_USERNAME}:{self.MONGO_PASSWORD}@{self.MONGO_HOST}/?retryWrites=true&w=majority")
         self.db = self.client[self.MONGO_TODO_APP_DATABASE]
+
+    # User management.
+
+    @property
+    def users(self):
+        users = []
+        raw_users = self.db['users'].find()
+        for raw_user in raw_users:
+            user = User(raw_user['_id'], raw_user['role'])
+            users.append(user)
+        return users
+
+    def user_exists(self, github_id):
+        github_id = int(github_id)
+        return self.db['users'].count_documents({'_id': github_id}) > 0
+
+    def get_user(self, github_id):
+        github_id = int(github_id)
+        if self.user_exists(github_id):
+            raw_user = self.db['users'].find_one({'_id': github_id})
+            return User(raw_user['_id'], raw_user['role'])
+        else:
+            ## TODO: Work out a more sensible return type/functionality.
+            return None
+
+    def add_user(self, github_id, role = "reader"):
+        github_id = int(github_id)
+        if self.user_exists(github_id):
+            message = f"""User '{github_id}' already exists in database. Use the edit_user_role method if you want to change their role."""
+        else:
+            user = User(github_id, role) # To check the validity of the role being assigned.
+            self.db['users'].insert_one({'_id': github_id, 'role': role})
+            message = f"""User '{github_id}' successfully added and assigned the role of {role}."""
+        return message
+
+    def edit_user_role(self, github_id, new_role):
+        github_id = int(github_id)
+        user = User(github_id, new_role) # To check the validity of the role being assigned.
+        if not self.user_exists(github_id):
+            message = "User '{github_id}' doesn't exist in database. Use the add_user method if you want to add a user."
+        else:
+            self.db['users'].update_one({'_id': github_id}, {'$set': {'_id': github_id, 'role': new_role}})
+            message = f"""User '{github_id}' successfully assigned the role of {new_role}."""
+        return message
+
+    def delete_user(self, github_id):
+        github_id = int(github_id)
+        if not self.user_exists(github_id):
+            message = f"""User '{github_id}' doesn't exist in database. Are you sure this is the user you wanted to delete?"""
+        else:
+            self.db['users'].delete_one({'_id': github_id})
+            message = f"""User '{github_id}' successfully deleted."""
+        return message
+
 
     # Item management.
 
