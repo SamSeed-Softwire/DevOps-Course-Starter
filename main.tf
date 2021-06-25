77d443b1a80d128ffc43b8fb43e79f380d6c1b82
Sam Seed <sam.seed@softwire.com>
2021-06-25 12:45:30 +0100

    refactor(TF): add prefix variable to start of resource names

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_cosmosdb_account" "main" {
-    name = "softwirepilot-samseed-projectexercise-cosmos"
+    name = "${var.prefix}-softwirepilot-samseed-projectexercise-cosmos"
     resource_group_name = data.azurerm_resource_group.main.name
     location = data.azurerm_resource_group.main.location
     offer_type = "Standard"
@@ -46,7 +46,7 @@ resource "azurerm_cosmosdb_account" "main" {
 }
 
 resource "azurerm_cosmosdb_mongo_database" "main" {
-    name                = "softwirepilot-samseed-projectexercise-mongodb"
+    name                = "${var.prefix}-softwirepilot-samseed-projectexercise-mongodb"
     resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
     account_name        = azurerm_cosmosdb_account.main.name
     lifecycle {
@@ -55,7 +55,7 @@ resource "azurerm_cosmosdb_mongo_database" "main" {
 }
 
 resource "azurerm_app_service_plan" "main" {
-    name = "terraformed-asp"
+    name = "${var.prefix}-terraformed-asp"
     location = data.azurerm_resource_group.main.location
     resource_group_name = data.azurerm_resource_group.main.name
     kind = "Linux"
@@ -69,7 +69,7 @@ resource "azurerm_app_service_plan" "main" {
     }
 }
 resource "azurerm_app_service" "main" {
-    name = "softwirepilot-samseed-projectexercise-app-service2"
+    name = "${var.prefix}-softwirepilot-samseed-projectexercise-app-service2"
     location = data.azurerm_resource_group.main.location
     resource_group_name = data.azurerm_resource_group.main.name
     app_service_plan_id = azurerm_app_service_plan.main.id
